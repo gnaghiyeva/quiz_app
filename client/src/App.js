78eb065aab88bb58b46a801837833498002e33a9
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2023-09-06 22:37:54 +0400

    user-login-ui

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import './App.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { ROUTES } from './routes/routes';
 import { AdminContextProvider } from './context/AdminContext';
+import { UserContextProvider } from './context/UserContext';
 
 function App() {
   const routes = createBrowserRouter(ROUTES)
   return (
     <>
       <AdminContextProvider>
-        <RouterProvider router={routes}>
+        <UserContextProvider>
+          <RouterProvider router={routes}>
 
-        </RouterProvider>
+          </RouterProvider>
+        </UserContextProvider>
       </AdminContextProvider>
     </>
   );
--- client/src/pages/Main/MainRoot.jsx
+++ client/src/pages/Main/MainRoot.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useUserContext } from '../../context/UserContext'
+import UserNavbar from '../../components/Main/UserNavbar'
+import { Outlet } from 'react-router-dom'
+import UserLogin from './UserLogin'
 
 const MainRoot = () => {
+  const [user] = useUserContext()
   return (
-    <div>MainRoot</div>
+    <>
+      {/* <Navbarr/>
+  <Outlet/> */}
+      <>
+        {
+          !user?.isAdmin ? (
+            <>
+              <UserNavbar/>
+              <Outlet />
+            </>
+          ) : (
+            <UserLogin />
+          )
+        }
+      </>
+    </>
   )
 }
 
